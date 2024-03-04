241a1703f2140bda539c9feed27ddf9417c2f688
Gabi <gabriela.silva.pt1@gmail.com>
2024-03-04 10:09:53 +0000

    Separated the graph

---
+++ graph/Graph.cpp
@@ -0,0 +1,175 @@
+#include "Graph.h"
+
+///*******************///
+/* GRAPH */
+
+
+    Vertex* Graph::findVertex(const std::string& code)
+    {
+        for (size_t i = 0; i <vertexSet.size(); i++)
+        {
+            if (vertexSet[i]->getCode() == code)
+            {
+                return vertexSet[i];
+            }
+            
+        }
+        return NULL;
+    }
+ 
+    bool Graph::removeVertex(Vertex* v){
+        auto it = std::find(vertexSet.begin(), vertexSet.end(), v);
+        if(it == vertexSet.end()){
+            return false;
+        }
+        vertexSet.erase(it);
+        return true;
+    }
+
+    std::vector<Vertex*> Graph::getVertexSet() const{return vertexSet;}
+
+
+
+
+Vertex* Graph::findCity(const std::string &name) const{
+    for(Vertex* v: vertexSet){
+        City* c = dynamic_cast<City*>(v);
+        if(c){
+            // It's a city!
+            if(c->getName() == name){
+                return v;
+            }
+        }
+    }
+    return NULL;
+}
+
+Vertex* Graph::findCity(const int &id) const {
+    for(Vertex* v: vertexSet){
+        City* c = dynamic_cast<City*>(v);
+        if(c){
+            // It's a city!
+            if(c->getId() == id){
+                return v;
+            }
+        }
+    }
+    return NULL;
+}
+
+
+bool Graph::addCity(std::string &name, int id, std::string &code, int demand, int population){
+    City* city = new City(name,id,code,demand, population);
+    vertexSet.push_back(city);
+    return true;
+}
+
+bool Graph::addReservoir(std::string &name, std::string &municipality, int id, std::string &code, int delivery){
+    Reservoir* reservoir = new Reservoir(name, municipality,id, code,delivery);
+    vertexSet.push_back(reservoir);
+    return true;
+}
+
+bool Graph::addStation(int id, std::string &code){
+    Station* station = new Station(id, code);
+    vertexSet.push_back(station);
+    return true;
+}
+
+
+    bool Graph::addEdge(const std::string& origin,const std::string& target, double capacity )
+    {
+        auto firstVertex=findVertex(origin);
+        auto secondVertex=findVertex(target);
+        if (secondVertex==NULL||firstVertex==NULL)
+        {
+            return 1;
+        }
+        auto edge= new Edge(firstVertex,secondVertex,capacity);
+        firstVertex->addOutgoingEdge(edge);
+        secondVertex->addIncomingEdge(edge);
+        
+return 0;
+    }
+    //this can't be here because it need to call a function from the vertex which isn't defined yet...
+    bool Graph::addBidirectionalEdge(const std::string& vertexA,const std::string& vertexB, double capacity){
+        auto firstVertex=findVertex(vertexA);
+        auto secondVertex=findVertex(vertexB);
+        if (secondVertex==NULL||firstVertex==NULL)
+        {
+            return 1;
+        }
+
+        auto edge1= new Edge(firstVertex,secondVertex,capacity);
+        auto edge2= new Edge(secondVertex,firstVertex,capacity);
+        edge1->setReverse(edge2);
+        edge2->setReverse(edge1); 
+        firstVertex->addOutgoingEdge(edge1);
+        secondVertex->addIncomingEdge(edge1);
+        firstVertex->addIncomingEdge(edge2);
+        secondVertex->addOutgoingEdge(edge2);
+return 0;
+    }
+
+
+
+///_____EDGE_____
+    Edge::Edge(Vertex *orig, Vertex *dest, double capacity){
+        this->orig = orig;
+        this->dest = dest;
+        this->capacity = capacity;
+    }
+
+    Vertex * Edge::getDest() const{return dest;}
+    double Edge::getCapacity() const{return capacity;}
+    bool Edge::isSelected() const{return selected;}
+    Vertex * Edge::getOrig() const{return orig;}
+    Edge *Edge::getReverse() const{return reverse;}
+    double Edge::getFlow() const{return flow;}
+
+    void Edge::setSelected(bool selected){this->selected = selected;}
+    void Edge::setReverse(Edge *reverse){this->reverse = reverse;}
+    void Edge::setFlow(double flow){this->flow = flow;};
+///____CITY_____
+
+    City::City(std::string &name, int id, std::string &code, int demand, int population) : Vertex(id,code){
+        this->name = name;
+        this->id = id;
+        this->demand = demand;
+        this->population = population;
+    }
+    char City::getType() { return 'c';}
+
+    std::string City::getName(){return name;}
+    int City::getDemand(){return demand;}
+///_____RESERVOIR_____
+    Reservoir::Reservoir(std::string &name, std::string &municipality, int id, std::string &code, int delivery) : Vertex(id,code){
+        this->name = name;
+        this->municipality = municipality;
+        this->delivery = delivery;
+    }
+    char Reservoir::getType() { return 'r';}
+
+///___STATION
+
+    Station::Station(int id, std::string &code) : Vertex(id,code){
+        
+    }
+    char Station::getType() { return 's';}
+///___VERTEX____
+    std::vector<Edge*> Vertex::getAdj(){return adj;}
+    Vertex::Vertex(int id,const std::string& code){this->id = id;
+    this->code = code;
+    }
+    int Vertex::getId() const{return id;};
+    const std::string& Vertex::getCode(){return code;}
+    bool Vertex::addOutgoingEdge(Edge* edge)
+    {
+        adj.push_back(edge);
+        return 0;
+    }
+    bool Vertex::addIncomingEdge(Edge* edge)
+    {
+        incoming.push_back(edge);
+        return 0;
+    }
--- graph/Graph.h
+++ graph/Graph.h
@@ -24,34 +24,11 @@ public:
     bool addReservoir(std::string &name, std::string &municipality, int id, std::string &code, int delivery);
     bool addStation(int id, std::string &code);
 
-    Vertex* findVertex(const std::string& code)
-    {
-        for (size_t i = 0; i <vertexSet.size(); i++)
-        {
-            if (vertexSet[i]->getCode() == code)
-            {
-                return vertexSet[i];
-            }
-            
-        }
-        return NULL;
-    }
+    Vertex* findVertex(const std::string& code);
     bool addEdge(const std::string& origin,const std::string& target, double capacity );
-    //this can't be here because it need to call a function from the vertex which isn't defined yet...
     bool addBidirectionalEdge(const std::string& vertexA,const std::string& vertexB, double capacity);
-
-    bool removeVertex(Vertex* v){
-        auto it = std::find(vertexSet.begin(), vertexSet.end(), v);
-        if(it == vertexSet.end()){
-            return false;
-        }
-        vertexSet.erase(it);
-        return true;
-    }
-
-    std::vector<Vertex*> getVertexSet() const{return vertexSet;}
-
-
+    bool removeVertex(Vertex* v);
+    std::vector<Vertex*> getVertexSet() const;
 };
 
 
@@ -67,32 +44,20 @@ protected:
     std::string code;
 
 public:
-    std::vector<Edge*> getAdj(){return adj;}
-    Vertex(int id,const std::string& code){this->id = id;
-    this->code = code;
-    }
-    int getId() const{return id;};
+    std::vector<Edge*> getAdj();
+    Vertex(int id,const std::string& code);
+    int getId() const;
     virtual char getType()=0;
-    const std::string& getCode(){return code;}
-    bool addOutgoingEdge(Edge* edge)
-    {
-        adj.push_back(edge);
-        return 0;
-    }
-    bool addIncomingEdge(Edge* edge)
-    {
-        incoming.push_back(edge);
-        return 0;
-    }
+    const std::string& getCode();
+    bool addOutgoingEdge(Edge* edge);
+    bool addIncomingEdge(Edge* edge);
 };
 
 class Station : public Vertex{
 
 public:
-    Station(int id, std::string &code) : Vertex(id,code){
-        
-    }
-    char getType() override{ return 's';}
+    Station(int id, std::string &code);
+    char getType() override;
 
 };
 
@@ -102,12 +67,8 @@ private:
     std::string municipality;
     int delivery;
 public:
-    Reservoir(std::string &name, std::string &municipality, int id, std::string &code, int delivery) : Vertex(id,code){
-        this->name = name;
-        this->municipality = municipality;
-        this->delivery = delivery;
-    }
-    char getType() override{ return 'r';}
+    Reservoir(std::string &name, std::string &municipality, int id, std::string &code, int delivery);
+    char getType() override;
 };
 
 
@@ -119,16 +80,11 @@ private:
     int demand;
     int population;
 public:
-    City(std::string &name, int id, std::string &code, int demand, int population) : Vertex(id,code){
-        this->name = name;
-        this->id = id;
-        this->demand = demand;
-        this->population = population;
-    }
-    char getType() override{ return 'c';}
+    City(std::string &name, int id, std::string &code, int demand, int population);
+    char getType() override;
 
-    std::string getName(){return name;}
-    int getDemand(){return demand;}
+    std::string getName();
+    int getDemand();
 
 };
 
@@ -148,22 +104,18 @@ protected:
 
     double flow; // for flow-related problems
 public:
-    Edge(Vertex *orig, Vertex *dest, double capacity){
-        this->orig = orig;
-        this->dest = dest;
-        this->capacity = capacity;
-    }
+    Edge(Vertex *orig, Vertex *dest, double capacity);
 
-    Vertex * getDest() const{return dest;}
-    double getCapacity() const{return capacity;}
-    bool isSelected() const{return selected;}
-    Vertex * getOrig() const{return orig;}
-    Edge *getReverse() const{return reverse;}
-    double getFlow() const{return flow;}
+    Vertex * getDest() const;
+    double getCapacity() const;
+    bool isSelected() const;
+    Vertex * getOrig() const;
+    Edge *getReverse() const;
+    double getFlow() const;
 
-    void setSelected(bool selected){this->selected = selected;}
-    void setReverse(Edge *reverse){this->reverse = reverse;}
-    void setFlow(double flow){this->flow = flow;};
+    void setSelected(bool selected);
+    void setReverse(Edge *reverse);
+    void setFlow(double flow);
 
 
 };
@@ -172,88 +124,6 @@ public:
 
 
 
-///*******************///
-/* GRAPH */
-
-Vertex* Graph::findCity(const std::string &name) const{
-    for(Vertex* v: vertexSet){
-        City* c = dynamic_cast<City*>(v);
-        if(c){
-            // It's a city!
-            if(c->getName() == name){
-                return v;
-            }
-        }
-    }
-    return NULL;
-}
-
-Vertex* Graph::findCity(const int &id) const {
-    for(Vertex* v: vertexSet){
-        City* c = dynamic_cast<City*>(v);
-        if(c){
-            // It's a city!
-            if(c->getId() == id){
-                return v;
-            }
-        }
-    }
-    return NULL;
-}
-
-
-bool Graph::addCity(std::string &name, int id, std::string &code, int demand, int population){
-    City* city = new City(name,id,code,demand, population);
-    vertexSet.push_back(city);
-    return true;
-}
-
-bool Graph::addReservoir(std::string &name, std::string &municipality, int id, std::string &code, int delivery){
-    Reservoir* reservoir = new Reservoir(name, municipality,id, code,delivery);
-    vertexSet.push_back(reservoir);
-    return true;
-}
-
-bool Graph::addStation(int id, std::string &code){
-    Station* station = new Station(id, code);
-    vertexSet.push_back(station);
-    return true;
-}
-
-
-    bool Graph::addEdge(const std::string& origin,const std::string& target, double capacity )
-    {
-        auto firstVertex=findVertex(origin);
-        auto secondVertex=findVertex(target);
-        if (secondVertex==NULL||firstVertex==NULL)
-        {
-            return 1;
-        }
-        auto edge= new Edge(firstVertex,secondVertex,capacity);
-        firstVertex->addOutgoingEdge(edge);
-        secondVertex->addIncomingEdge(edge);
-        
-return 0;
-    }
-    //this can't be here because it need to call a function from the vertex which isn't defined yet...
-    bool Graph::addBidirectionalEdge(const std::string& vertexA,const std::string& vertexB, double capacity){
-        auto firstVertex=findVertex(vertexA);
-        auto secondVertex=findVertex(vertexB);
-        if (secondVertex==NULL||firstVertex==NULL)
-        {
-            return 1;
-        }
-
-        auto edge1= new Edge(firstVertex,secondVertex,capacity);
-        auto edge2= new Edge(secondVertex,firstVertex,capacity);
-        edge1->setReverse(edge2);
-        edge2->setReverse(edge1); 
-        firstVertex->addOutgoingEdge(edge1);
-        secondVertex->addIncomingEdge(edge1);
-        firstVertex->addIncomingEdge(edge2);
-        secondVertex->addOutgoingEdge(edge2);
-return 0;
-    }
 
 
 #endif //WM_GRAPH_H
