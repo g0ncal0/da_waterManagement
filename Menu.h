d9b6ee97d59166827333ff459f96ab0232143ccc
Gabi <gabriela.silva.pt1@gmail.com>
2024-03-04 10:11:51 +0000

    Separated the menu

--- Menu.h
+++ Menu.h
@@ -12,39 +12,12 @@
 class Menu {
 private:
     public:
-    static std::vector<std::string> getOptions(){
-        return {"Max Amount of Water by city", "Are needs met?", "Balance the load", "[RELIABILITY] One water reservoir is out", "[RELIABILITY] Which pumping stations are essential", "[RELIABILITY] Which pipelines are essential by city?", "[RELIABILITY] What cities are affected by mal-functioning of a pipeline."};
-    }
-    static void print(std::string t){
-        std::cout << t << "\n";
-    }
-    static void printList(std::vector<std::string> v){
-        for(int i = 0; i < v.size(); i++){
-            std::cout << i << " : " << v[i];
-        }
-    }
-    static void displayoptions(){
-        print("Options:");
-        printList(getOptions());
-    }
-    static int getNumber() {
-        int n = -1;
-        while (n != -1) {
-            try {
-                std::cin >> n;
-            } catch (...) {
-                std::cout << "error. try again";
-            }
-        }
-        return n;
-    }
-    static int chooseoption(){
-        int choosen = getOptions().size() + 1;
-        while(choosen >= getOptions().size() || choosen < 0){
-            choosen = getNumber();
-        }
-        return choosen;
-    }
+    static std::vector<std::string> getOptions();
+    static void print(std::string t);
+    static void printList(std::vector<std::string> v);
+    static void displayoptions();
+    static int getNumber();
+    static int chooseoption();
 };
 
 
